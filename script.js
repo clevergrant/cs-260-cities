6f86ad907fcfe5300da74bd660b60ecb34d8329c
clevergrant <clevergrant@gmail.com>
2019-09-27 10:19:03 -0600

    starting

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById(`cityField`).addEventListener(`keyup`, event => {
+	event.preventDefault()
+	const url = `http://bioresearch.byu.edu/cs260/jquery/getcity.cgi?q=P`
+	fetch(url)
+		.then(res => res.json()).then(json => {
+
+			console.log(json)
+			console.log(json[0])
+			console.log(`Got ` + json[0].city)
+
+			let everything
+			everything = `<ul>`
+
+			for (let i = 0; i < json.length; i++)
+				everything += `<li> ` + json[i].city
+
+			everything += `</ul>`
+			document.getElementById(`txtHint`).innerHTML = everything
+		})
+})
